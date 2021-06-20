98e5cdb60f5a6b3d7db8a9eeea3271169620f6ad
Rabyn700 <rabyn900@gmail.com>
2021-06-20 10:25:34 +0545

    add commit

---
+++ index.js
@@ -0,0 +1,42 @@
+let given = document.getElementById('field');
+let buttons = document.querySelectorAll('button');
+
+
+let givenInput = '';
+
+for(but of buttons){
+
+but.addEventListener('click', (e) => {
+ buttonText= e.target.innerText;
+
+if(buttonText == 'X'){
+  buttonText = '*';
+ givenInput += buttonText;
+ given.value = givenInput;
+
+}else if(buttonText == 'GET RESULT'){
+ 
+ given.value =  eval(given.value);
+
+}else if(buttonText == 'C'){
+  given.value = '';
+  givenInput = '';
+}else if(buttonText == 'DL'){
+
+  given.value = given.value.substring(0, given.value.length - 1);
+  
+  givenInput = given.value.substring(0, given.value.length + 1);
+
+
+}else{
+  if(buttonText !== 'DL'){
+    givenInput += buttonText;
+    given.value = givenInput;
+  }
+
+}
+
+});
+
+
+}
